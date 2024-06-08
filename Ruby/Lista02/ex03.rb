7b23bcfa0f0be073869b7ac937dbefb9c6f3af2f
paularcsarruda <paularcsarruda@gmail.com>
2024-06-08 16:36:11 -0300

    Logica e Banco de Dados

--- Ruby/Lista02/ex03.rb
+++ Ruby/Lista02/ex03.rb
@@ -4,3 +4,26 @@
 # Além disso, sabendo que para aprovação o aluno precisa ter uma média anual maior ou igual a 7,
 # escreva uma mensagem indicando se o aluno foi aprovado ou reprovado.
 
+
+puts("Digite a primeira média: ")
+primeira_media = gets.to_f
+
+puts("Digite a segunda média: ")
+segunda_media = gets.to_f
+
+puts("Digite a terceira média: ")
+terceira_media = gets.to_f
+
+puts("Digite a quarta média: ")
+quarta_media = gets.to_f
+
+# Soma dos pesos
+soma_pesos = 1 + 2 + 3 + 4
+
+media_final = (primeira_media * 1 + segunda_media * 2 + terceira_media * 3 + quarta_media * 4) / soma_pesos
+
+if media_final >= 7
+  puts("Média Anual: #{media_final.round(2)}. Aluno Aprovado.")
+else
+  puts("Média Anual: #{media_final.round(2)}. Aluno Reprovado.")
+end
---
+++ Ruby/Lista02/ex04.rb
@@ -0,0 +1,6 @@
+# Escreva um programa para ler um valor (em reais), calcular e exibir quantas cédulas de 100, 50, 20, 10, 5, 2 e 1
+# são necessárias. [Dica: utilize o operador “%” (mod)]
+
+puts("Informe o valor que deseja sacar R$ ")
+cedulas = gets.chomp.to_f
+
