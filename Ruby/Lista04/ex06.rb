a1bb5add8423a356d195d1267bd2e177e7c81e81
paularcsarruda <paularcsarruda@gmail.com>
2024-06-10 21:13:54 -0300

    Logica e Banco de Dados

--- Ruby/Lista04/ex06.rb
+++ Ruby/Lista04/ex06.rb
@@ -3,3 +3,21 @@ Desenvolva um programa que faça a tabuada de um número qualquer inteiro que se
 não deve necessariamente iniciar em 1 e terminar em 10, o valor inicial e final devem ser informados também pelo usuário.
 =end
 
+loop do
+  puts("Montar a tabuada de: ")
+  x = gets.chomp.to_i
+  puts("Começar por: ")
+  y = gets.chomp.to_i
+  puts("Terminar em: ")
+  z = gets.chomp.to_i
+
+  if z < y
+    puts("O número inicial não pode ser maior que o final. Tente novamente.")
+  else
+    puts("Vou montar a tabuada de #{x} começando por #{y} e finalizando em #{z}:")
+    for n in y..z do
+      puts("#{x} x #{n} = #{x * n}")
+    end
+    break
+  end
+end
